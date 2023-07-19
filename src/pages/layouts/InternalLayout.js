2156629c43823b6f2824815958f76eba0c729fda
leraaj <leraalfredojohn@gmail.com>
2023-07-19 19:14:44 +0800

    sidebar updated

--- src/pages/layouts/InternalLayout.js
+++ src/pages/layouts/InternalLayout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavigationBar from "../../components/shared/NavigationBar";
 import { Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 const InternalLayout = () => {
   const containerStyle = {
     marginTop: "3.625rem",
@@ -19,13 +20,13 @@ const InternalLayout = () => {
   ];
   const sidebarLists = [
     {
-      navId: 1,
+      id: 1,
       redirectTo: "admin/accountSettings",
       name: "Accounts Settings",
     },
-    { navId: 2, redirectTo: "admin/clients", name: "Applicants" },
-    { navId: 3, redirectTo: "admin/applicants", name: "Client" },
-    { navId: 4, redirectTo: "admin/posts", name: "Posts" },
+    { id: 2, redirectTo: "admin/clients", name: "Applicants" },
+    { id: 3, redirectTo: "admin/applicants", name: "Client" },
+    { id: 4, redirectTo: "admin/posts", name: "Posts" },
   ];
   const contentHeight = {
     height: "calc(100vh - 3.625rem)",
@@ -36,8 +37,26 @@ const InternalLayout = () => {
       <div className="bg-light">
         <NavigationBar links={accessLists} position="true" />
         <div className="row flex-nowrap mx-0" style={containerStyle}>
-          <aside className="col-2 col-sm-3 col-md-3 col-lg-3 col-xl-2 px-sm-2 px-0 bg-dark position-fixed vh-100 text-light container">
-            Sidebar
+          <aside className="col-2 col-sm-3 col-md-3 col-lg-3 col-xl-2 px-sm-2 px-0 bg-dark position-fixed vh-100 text-light container pt-4 px-1 ">
+            <ul class="list-group">
+              {sidebarLists.map((link) => {
+                return (
+                  <>
+                    <li
+                      key={link.id}
+                      class="list-group-item bg-transparent border border-0"
+                    >
+                      <Link
+                        to={link.redirectTo}
+                        className="link-light text-decoration-none"
+                      >
+                        {link.name}
+                      </Link>
+                    </li>
+                  </>
+                );
+              })}
+            </ul>
           </aside>
           <section
             className="col-10 col-sm-9 col-md-9 col-lg-9 col-xl-10 pt-4 px-5 offset-2 offset-sm-3 offset-md-3 offset-lg-3 offset-xl-2 "
