184dfdc67c6cfa7bda5aa4c5466ffa370bd59e77
leraaj <leraalfredojohn@gmail.com>
2023-08-19 13:44:27 +0800

    syncing back

--- src/pages/external/login/Index.js
+++ src/pages/external/login/Index.js
@@ -7,7 +7,7 @@ const Index = () => {
   const navigate = useNavigate();
   const [usernameValue, setUsernameValue] = useState(null);
   const [passwordValue, setPasswordValue] = useState(null);
-  const apiEndpoint = "https://darkshot-server.onrender.com/api/user/login";
+  const apiEndpoint = "http://localhost:3001/api/user/login";
   const loginContent = {
     minHeight: "100vh",
     width: "100vw",
